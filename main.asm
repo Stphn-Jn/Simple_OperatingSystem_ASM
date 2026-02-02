2fc3a6687b947db3b96f9d35016a833aa8456074
Stphn-Jn <amihan.jan.stephen@gmail.com>
2026-02-02 22:33:58 +0800

    VGA Graphics Mode Setup and Pixel Drawing in Assembly

--- main.asm
+++ main.asm
@@ -1,22 +1,20 @@
 [org 0x7c00]
 
-mov si, WELCOME_MSG    ; Point SI register to our string label
-call print_string      ; Call our new print function
+; 1. Switch to Video Mode
+mov ah, 0x00    ; Set video mode function
+mov al, 0x13    ; Mode 13h: 320x200 graphics, 256 colors
+int 0x10        ; BIOS video interrupt
 
-jmp $                  
+; 2. Draw a Pixel
+; We use Interrupt 10h, Function 0Ch
+mov ah, 0x0c    ; Write graphics pixel function
+mov al, 0x0e    ; Color: Yellow (Color index 14)
+mov bh, 0x00    ; Page number
+mov cx, 160     ; X coordinate (Center: 320 / 2)
+mov dx, 100     ; Y coordinate (Center: 200 / 2)
+int 0x10
 
-print_string:
-    mov ah, 0x0e       ; BIOS teletype mode
-.loop:
-    lodsb              
-    cmp al, 0          
-    je .done           
-    int 0x10           
-    jmp .loop          
-.done:
-    ret
-
-WELCOME_MSG: db 'Welcome to PingOS!', 0
+jmp $           ; Infinite loop
 
 times 510-($-$$) db 0
 dw 0xaa55
