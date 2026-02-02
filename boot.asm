f57cc716792673898c2c096544c1a44502c50f99
Stphn-Jn <amihan.jan.stephen@gmail.com>
2026-02-02 23:16:02 +0800

    Refactor bootloader and kernel assembly code for improved structure and command handling

--- boot.asm
+++ boot.asm
@@ -1,26 +1,27 @@
 [org 0x7c00]
-KERNEL_OFFSET equ 0x1000    ; Memory address where we will load the kernel
+KERNEL_OFFSET equ 0x1000
 
-mov [BOOT_DRIVE], dl        ; BIOS stores boot drive info in DL; save it
-
-; 1. Set up the Stack (important for calling functions)
+mov [BOOT_DRIVE], dl
 mov bp, 0x9000
 mov sp, bp
 
-; 2. Read Kernel from Disk
-mov ah, 0x02                ; BIOS read sectors function
-mov al, 1                   ; Number of sectors to read (increase as kernel grows)
-mov ch, 0x00                ; Cylinder 0
-mov dh, 0x00                ; Head 0
-mov cl, 0x02                ; Sector 2 (Sector 1 is this bootloader)
-mov dl, [BOOT_DRIVE]        ; Read from the drive we booted from
-mov bx, KERNEL_OFFSET       ; Destination address in RAM
-int 0x13                    ; Disk Controller Interrupt
+disk_reset:
+    mov ah, 0
+    mov dl, [BOOT_DRIVE]
+    int 0x13
+    jc disk_reset
 
-jc disk_error               ; If carry flag is set, there was an error
+mov ah, 0x02
+mov al, 10
+mov ch, 0
+mov dh, 0
+mov cl, 2
+mov dl, [BOOT_DRIVE]
+mov bx, KERNEL_OFFSET
+int 0x13
 
-; 3. Jump to Kernel
-jmp KERNEL_OFFSET           ; Transfer control to your kernel code
+jc disk_error
+jmp KERNEL_OFFSET
 
 disk_error:
     mov ah, 0x0e
--- kernel.asm
+++ kernel.asm
@@ -1,52 +1,158 @@
 [org 0x1000]
 
-; 1. Enter Graphics Mode 13h
-mov ah, 0x00
-mov al, 0x13
+; 1. Enter Text Mode
+mov ax, 0x03
 int 0x10
 
-; 2. Draw a "Taskbar" at the bottom
-; Parameters for draw_rect: X, Y, Width, Height, Color
-mov cx, 0       ; X
-mov dx, 180     ; Y (Near bottom)
-mov si, 320     ; Width
-mov di, 20      ; Height
-mov al, 0x07    ; Color: Light Gray
-call draw_rect
+; 2. Print Welcome
+mov si, welcome_msg
+call print_string
 
-; 3. Draw a "Start Button"
-mov cx, 5       ; X
-mov dx, 182     ; Y
-mov si, 40      ; Width
-mov di, 16      ; Height
-mov al, 0x01    ; Color: Blue
-call draw_rect
+cli_loop:
+    mov si, prompt
+    call print_string
+    call get_input
 
-jmp $           ; Halt CPU
+    ; Command Handling
+    mov si, input_buffer
+    mov di, cmd_cfdisk
+    call strcmp
+    jc .do_cfdisk
 
-; --- GUI FUNCTIONS ---
+    mov si, input_buffer
+    mov di, cmd_install
+    call strcmp
+    jc .do_install
 
-; Function: draw_rect
-; Inputs: cx=X, dx=Y, si=Width, di=Height, al=Color
-draw_rect:
-    pusha               ; Save all registers
-    add si, cx          ; si now holds the right boundary (X + Width)
-    add di, dx          ; di now holds the bottom boundary (Y + Height)
-    mov bx, dx          ; Save starting Y in bx
+    ; Check for empty input (just Enter)
+    cmp byte [input_buffer], 0
+    je cli_loop
 
-.row_loop:
-    push cx             ; Save starting X
-.col_loop:
-    mov ah, 0x0c        ; BIOS write pixel
+    mov si, unknown_msg
+    call print_string
+    jmp cli_loop
+
+.do_cfdisk:
+    mov si, cfdisk_msg
+    call print_string
+    mov byte [is_partitioned], 1
+    jmp cli_loop
+
+.do_install:
+    cmp byte [is_partitioned], 0
+    je .no_disk
+    mov si, install_msg
+    call print_string
+    mov cx, 5
+.loop:
+    mov ah, 0x0e
+    mov al, '.'
     int 0x10
-    inc cx
-    cmp cx, si          ; Check if row is finished
-    jne .col_loop
-    
-    pop cx              ; Reset X to start of row
-    inc dx              ; Move to next Y line
-    cmp dx, di          ; Check if all rows are finished
-    jne .row_loop
+    push cx
+    mov cx, 0xFFFF
+    .delay: loop .delay
+    pop cx
+    loop .loop
+    mov si, done_msg
+    call print_string
+    jmp cli_loop
 
-    popa                ; Restore all registers
+.no_disk:
+    mov si, err_nodisk
+    call print_string
+    jmp cli_loop
+
+reboot:
+    jmp 0xFFFF:0000
+
+; --- UPDATED CLI UTILITIES ---
+
+get_input:
+    mov di, input_buffer
+    mov cx, 0           ; Character counter
+
+.loop:
+    mov ah, 0x00
+    int 0x16            ; Get key
+
+    cmp al, 0x0D        ; ENTER key
+    je .done
+
+    cmp al, 0x08        ; BACKSPACE key
+    je .backspace
+
+    cmp cx, 63          ; Prevent buffer overflow
+    je .loop
+
+    ; Regular character
+    mov ah, 0x0e
+    int 0x10            ; Echo
+    stosb               ; Store in buffer
+    inc cx
+    jmp .loop
+
+.backspace:
+    cmp cx, 0           ; Can't backspace if buffer is empty
+    je .loop
+    
+    dec di              ; Move buffer pointer back
+    dec cx              ; Decrease count
+    
+    ; Visual Erasure: Back-Space-Back
+    mov ah, 0x0e
+    mov al, 0x08        ; Move cursor back
+    int 0x10
+    mov al, ' '         ; Print space to "erase"
+    int 0x10
+    mov al, 0x08        ; Move cursor back again
+    int 0x10
+    jmp .loop
+
+.done:
+    mov al, 0
+    stosb               ; Null terminate
+    mov si, newline
+    call print_string
     ret
+
+print_string:
+    mov ah, 0x0e
+.lp:
+    lodsb
+    cmp al, 0
+    je .dn
+    int 0x10
+    jmp .lp
+.dn: ret
+
+strcmp:
+.loop:
+    mov al, [si]
+    mov bl, [di]
+    cmp al, bl
+    jne .not_equal
+    cmp al, 0
+    je .equal
+    inc si
+    inc di
+    jmp .loop
+.not_equal:
+    clc
+    ret
+.equal:
+    stc
+    ret
+
+; --- DATA ---
+is_partitioned db 0
+newline     db 13, 10, 0
+welcome_msg db 'Arch-PingOS Live ISO', 13, 10, 'Commands: cfdisk, pacman', 13, 10, 0
+prompt      db '[root@pingos]# ', 0
+cmd_cfdisk  db 'cfdisk', 0
+cmd_install db 'pacman', 0
+cfdisk_msg  db 'Partitioning... OK', 13, 10, 0
+install_msg db 'Installing system', 0
+done_msg    db ' [OK]', 13, 10, 'Done! Press "r" to reboot.', 13, 10, 0
+unknown_msg db 'Unknown command.', 13, 10, 0
+err_nodisk  db 'Run cfdisk first!', 13, 10, 0
+input_buffer times 64 db 0
